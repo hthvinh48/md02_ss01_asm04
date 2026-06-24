ffe2fe67470b3b0f6e7ba3a82c9a54eb78c432a4
Hiển Vinh <hthvinh48@gmail.com>
2026-06-24 14:59:41 +0700

    minor update

--- src/Ss01Asm04.java
+++ src/Ss01Asm04.java
@@ -2,19 +2,37 @@ import java.util.Scanner;
 
 public class Ss01Asm04 {
     public static void main(String[] args) {
-        float width, height, area, perimeter;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Nhập chiều rộng hình chữ nhật: ");
-        width = Float.parseFloat(sc.next());
-        System.out.print("Nhập chiều cao hình chữ nhật: ");
-        height = Float.parseFloat(sc.next());
+
+        float width = 0;
+        float height = 0;
+
+        while (true) {
+            try {
+                System.out.print("Nhập chiều rộng hình chữ nhật: ");
+                width = Float.parseFloat(sc.next());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Dữ liệu không hợp lệ. Vui lòng nhập số!");
+            }
+        }
+
+        while (true) {
+            try {
+                System.out.print("Nhập chiều cao hình chữ nhật: ");
+                height = Float.parseFloat(sc.next());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Dữ liệu không hợp lệ. Vui lòng nhập số!");
+            }
+        }
+
+        float area = width * height;
+        float perimeter = 2 * (width + height);
+
+        System.out.printf("Diện tích: %.2f%n", area);
+        System.out.printf("Chu vi: %.2f%n", perimeter);
+
         sc.close();
-
-        area = width * height;
-        perimeter = 2 * (width + height);
-
-        System.out.printf("Diện tích: %.2f", Math.round(area * 100) / 100.0);
-        System.out.println();
-        System.out.printf("Chu vi: %.2f", Math.round(perimeter * 100) / 100.0);
     }
 }
